e19f24c02737a24dedfa794a403c11dbdc100f05
Phaze-III <xj.dropbox+github@gmail.com>
2020-05-25 14:12:50 +0200

    Add missing Reset Reasons

--- util/check-status-page-strings.sh
+++ util/check-status-page-strings.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-SrcDir="${SrcBase:-..}/sensors-software/airrohr-firmware"
+SrcDirAirrohr="${SrcBase:-..}/sensors-software/airrohr-firmware"
+SrcDirArduino="${SrcBase:-..}/Arduino/cores/esp8266"
 
-if [ ! -r "${SrcDir}" ]
+if [ ! -r "${SrcDirArduino}" ]
 then
-   echo Please set SrcDir
-   exit 1
+   echo Please set SrcDirArduino
+else
+   echo "### Reset Reasons:"
+   grep -h 'case REASON_' "${SrcDirArduino}"/*.cpp
 fi
 
-echo "### Uptime related human readable strings:"
-grep -h 'PSTR.*time_ms' "${SrcDir}"/*.ino "${SrcDir}"/*.cpp
+if [ ! -r "${SrcDirAirrohr}" ]
+then
+   echo Please set SrcDirAirrohr
+else
+   echo "### Uptime related human readable strings:"
+   grep -h 'PSTR.*time_ms' "${SrcDirAirrohr}"/*.ino "${SrcDirAirrohr}"/*.cpp
 
-echo "### INTL_PARAMETER string prefixes for get-sensor-status.sh"
-grep 'INTL_PARAMETER' "${SrcDir}"/intl_??.h \
-   | cut -d\" -f2 | cut -c1-3 | sort | uniq -c
+   echo "### INTL_PARAMETER string prefixes for get-sensor-status.sh"
+   grep 'INTL_PARAMETER' "${SrcDirAirrohr}"/intl_??.h \
+      | cut -d\" -f2 | cut -c1-3 | sort | uniq -c
 
-echo "### List of languages:"
-echo $(ls -1 "${SrcDir}"/intl_??.h | sed -e 's/^.*intl_\(..\).h.*/\1/')
+   echo "### List of languages:"
+   echo $(ls -1 "${SrcDirAirrohr}"/intl_??.h | sed -e 's/^.*intl_\(..\).h.*/\1/')
 
-echo "### List of INTL_* strings for Grafana Dashboard:"
-egrep 'INTL_(ERROR|NUMBER_OF_MEASUREMENT|TIME_SENDING)' "${SrcDir}"/intl_??.h \
-   | sed -e 's/^.*intl_//' -e 's/.h:#define//'
+   echo "### List of INTL_* strings for Grafana Dashboard:"
+   egrep 'INTL_(ERROR|NUMBER_OF_MEASUREMENT|TIME_SENDING)' "${SrcDirAirrohr}"/intl_??.h \
+      | sed -e 's/^.*intl_//' -e 's/.h:#define//'
+fi
